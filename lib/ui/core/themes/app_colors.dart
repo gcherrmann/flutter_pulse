93dcdb3870db9a1c22546ecebf1125d2bd4c8eb7
gcherrmann <gcherrmann007@gmail.com>
2025-09-18 11:48:29 -0300

    Splash e Cores
    Adicionei todos os assets redimensionei a imagem da splash

--- lib/ui/core/themes/app_colors.dart
+++ lib/ui/core/themes/app_colors.dart
@@ -2,12 +2,21 @@ import 'dart:ui';
 
 class AppColors {
   static const white = Color(0xFFFFFFFF);
-  static const lightGray = Color(0xFFF3F3F3);
-  static const darkGray = Color(0xFF484848);
-  static const red = Color(0xFFE40C23);
-  static const darkRed = Color(0xFF960818);
-  static const green = Color(0xFF4CAF50);
-  static const intermediaryGray = Color(0xFFD6D6D6);
-  static const mediumGray = Color(0xFF8C8C8C);
-  static const blue = Color(0xFF548EFF);
+  static const lightGray = Color(0xFFB8C4D0);
+  static const notificationDateBackground = Color(0xFFCCD3DC);
+  static const notificationDateText = Color(0xFF193C5B);
+  static const pageBackground = Color(0x99F4F6F9);
+  static const tabBarIconOutline = Color(0xFFBFCAD9);
+  static const green = Color(0xFF41C735);
+  static const green10 = Color(0x1A41C735);
+  static const yellow = Color(0xFFF5E22F);
+  static const orange = Color(0xFFF17B48);
+  static const purple = Color(0xFFCC29CF);
+  static const red = Color(0xFFE05151);
+  static const red10 = Color(0x1AE05151);
+  static const blue = Color(0xFF1B69B0);
+  static const boldFontColor = Color(0xFF1A3C5B);
+  static const boldFontColor70 = Color(0xB31A3C5B);
+  static const regularFontColor = Color(0xFF1C3044);
+  static const regularFontColorSecondary = Color(0xFF262C32);
 }
